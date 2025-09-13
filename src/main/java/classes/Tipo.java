c40947082fb82e500c6cf0d9e4a06f64d4c649c7
Pedro Baleroni <pbaleroni@gmail.com>
2025-09-12 21:32:25 -0300

    add Tipo

---
+++ src/main/java/classes/Tipo.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package classes;
+
+/**
+ *
+ * @author pedro
+ */
+public class Tipo {
+    int indice;
+    String nome;
+    int fraqueza;
+    int resistencia;
+    
+    public Tipo(int indice, String nome){
+        this.indice = indice;
+        this.nome = nome;
+    }
+    
+    public Tipo (int indice, String nome, int fraqueza, int resistencia){
+        this.indice =indice;
+        this.nome = nome;
+        this.fraqueza = franqueza;
+        this.resistencia = resistencia;
+    }
+    
+    
+    
+    
+    
+}
