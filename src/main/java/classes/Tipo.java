52d83a564c79d87244c5fe3e12e0819824c7d792
Pedro Baleroni <pbaleroni@gmail.com>
2025-09-19 21:46:18 -0300

    commit de correcao

--- src/main/java/classes/Tipo.java
+++ src/main/java/classes/Tipo.java
@@ -3,34 +3,99 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package classes;
+import java.util.List; //Interface
+import java.util.ArrayList; // Funcional
 
 /**
  *
  * @author pedro
  */
+
+  // Instância da Classe
 public class Tipo {
     private int indice;
     private String nome;
-    private int fraqueza;
-    private int resistencia;
-    
+    private List<Tipo> fraquezas;
+    private List<Tipo> resistencias;
+
+   // Construtor da Classe
     public Tipo(int indice, String nome){
         this.indice = indice;
         this.nome = nome;
     }
-    
-    public Tipo (int indice, String nome, int fraqueza, int resistencia){
+
+    // Sobrecarga (OverLoad) do Construtor
+    // Mesma função recebendo funções parametrizadas de forma diferente.  
+    public Tipo (int indice, String nome, Tipo fraqueza, Tipo resistencia){
         this.indice =indice;
         this.nome = nome;
-        this.fraqueza = fraqueza;
-        this.resistencia = resistencia;
+        this.fraquezas = new ArrayList();
+        this.fraquezas.add(fraqueza);
+        this.resistencias = new ArrayList();
+        this.resistencias.add(resistencia);
     }
+
+    /**
+     *  Getters
+     *  Funções de Busca de atributos da Classe
+     *  Obrigatório caso:
+     *  - Utilize o atributo fora da Classe
+     *  - Utilize o atributo PRIVADO (PRIVATE) 
+     * @param nome
+     */
     
     public String getNome(){
         return this.nome;
     }
     
+    public String getFraquezas(){
+        String fraquezas = "";
+        for (Tipo arnaldo: this.fraquezas){
+            fraquezas += arnaldo.getNome() + " ";
+        }
+        return fraquezas;
+    }
+    public void addFraquezas(List<Tipo> fraquezas){
+        for(Tipo t: fraquezas){
+            this.fraquezas.add(t);
+        }
+    }
     
+    public String getResistencias(){
+        String resistencias = "";
+        for (Tipo t: this.resistencias){
+            resistencias += t.getNome() + " ";
+        }
+        return resistencias;
+    }
+    
+    public void addResistencias(List<Tipo> resistencias){
+        for(Tipo t: resistencias){
+            this.resistencias.add(t);
+        }
+    }
+    
+    /**
+     * Setters
+     * Funções de atribuição de valores de valores de atributos da classe
+     * Utilizado quando o atributo é privado
+     * @param nome
+     */
+    
+    public void setFraquezas(List<Tipo> fraquezas){
+        this.fraquezas = new ArrayList();
+        for(Tipo t: fraquezas){
+        this.fraquezas.add(t);
+        }
+    }
+    
+    
+    public void setResistencias(List<Tipo> resistencias){
+        this.resistencias = new ArrayList();
+        for(Tipo t: resistencias){
+        this.resistencias.add(t);
+        }
+    }
     
     
     
