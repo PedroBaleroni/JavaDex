7e378c9f2e44502d812bc23861d4e19940c7f6bd
Pedro Baleroni <pbaleroni@gmail.com>
2025-10-15 20:21:09 -0300

    atualiza Seeder

--- src/main/java/classes/main.java
+++ src/main/java/classes/main.java
@@ -15,14 +15,12 @@ public class main {
 
     public static void main(String[] args) {
         //Pokemon(Numero, Nome, Tipo1, Tipo2?, vida, peso, altura, evolui)
-            //database bd = new database();
-            //List<Pokemon> pokemons;
-            //Pokemon achado = bd.buscaPokemon(25);
-            //System.out.println(achado);
-           
-            Pokemon pikachu = new Pokemon(25, "Pikachu",tipo_do_pokemon, 35, 6.0f, 40,true);//Pedro Baleroni
-
-            System.out.println(pikachu.getTipoFraquezas());
+            database bd = new database();
+            bd.inicialize();
+            Pokemon pikachu = bd.buscaPokemon("Diglett");
+            
+          
+            System.out.println(pikachu.getEvoluiPara().getNome());
             
         }
 }
